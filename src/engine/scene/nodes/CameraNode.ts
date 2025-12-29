4cb5ccac69f43adb5190641abc5c8c4103057a45
peabnuts123 <peabnuts123@gmail.com>
2025-12-30 10:54:11 +1300

    Fix camera to work in transform hierarchies

--- src/engine/scene/nodes/CameraNode.ts
+++ src/engine/scene/nodes/CameraNode.ts
@@ -1,10 +1,10 @@
-import { glMatrix, mat4, quat, vec3 } from "gl-matrix";
+import { mat4, quat, vec3 } from "gl-matrix";
 
 import { Vector3 } from "@polyzone/engine/util/vector";
 import { Scene, SceneNode } from "@polyzone/engine/scene";
 import type { Ubo } from "@polyzone/engine/materials/Ubo";
-import { Flags } from "src/util/constants";
 import { Quaternion } from "@polyzone/engine/util/quaternion";
+import { DegreesToRadians } from "@polyzone/engine/util/math";
 
 export const CameraUboPropertyNames = ['viewProjectionMatrix'] as const;
 export type CameraUboPropertyName = (typeof CameraUboPropertyNames)[number];
@@ -43,40 +43,22 @@ export class CameraNode extends SceneNode {
   }
 
   public pointAt(target: Vector3): void {
-    const direction = this.position.subtract(target).normalizeSelf();
+    const direction = this.absolutePosition.subtract(target).normalizeSelf();
     const right = direction.cross(Vector3.up());
     const up = right.cross(direction);
-    this.rotation.set(Quaternion.fromLookDirection(direction, up));
+    this.absoluteRotation.set(Quaternion.fromLookDirection(direction, up));
   }
 
   private recalculateViewProjectionMatrix(): void {
-    if (Flags.UseEulernion) {
-      quat.set(
-        this._rotationTmp,
-        this.rotation.q.x,
-        this.rotation.q.y,
-        this.rotation.q.z,
-        this.rotation.q.w,
-      );
-    } else {
-      quat.fromEuler(
-        this._rotationTmp,
-        this.rotation.x,
-        this.rotation.y,
-        this.rotation.z,
-      );
-    }
-    vec3.set(this._positionTmp, this.position.x, this.position.y, this.position.z);
-    mat4.fromRotationTranslation(
+    mat4.invert(
       this._viewMatrixTmp,
-      this._rotationTmp,
-      this._positionTmp,
+      this.worldMatrix,
     );
-    mat4.invert(this._viewMatrixTmp, this._viewMatrixTmp);
 
+    // @TODO cache this matrix
     mat4.perspective(
       this._projectionMatrixTmp,
-      glMatrix.toRadian(this.fov),
+      this.fov * DegreesToRadians,
       this.aspectRatio,
       this.near,
       this.far,
