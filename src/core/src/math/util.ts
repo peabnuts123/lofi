821aba1a91a779046a834f30dfcd2b55c1bea159
peabnuts123 <peabnuts123@gmail.com>
2026-05-04 14:41:04 +1200

    Add some math utilities

--- src/core/src/math/util.ts
+++ src/core/src/math/util.ts
@@ -1,7 +1,11 @@
 /* @TODO Rename to `utils.ts` */
 
-export const DegreesToRadians = 1 / 180 * Math.PI;
-export const RadiansToDegrees = 1 / Math.PI * 180;
+export const Pi = Math.PI;
+export const Tau = Pi * 2;
+
+export const DegreesToRadians = 1 / 180 * Pi;
+export const RadiansToDegrees = 1 / Pi * 180;
+
 
 /**
  * Better modulus operation than built-in javascript `%` operator
@@ -41,4 +45,25 @@ export function clamp(value: number, min: number, max: number): number {
 
 export function clamp01(value: number): number {
   return clamp(value, 0, 1);
+}
+
+export function sinDegrees(value: number): number {
+  return Math.sin(value / 360 * Tau);
+}
+export function sin(value: number, period: number, min: number, max: number): number {
+  return ((Math.sin(value / period * Tau) + 1) / 2) * (max - min) + min;
+}
+
+export function cosDegrees(value: number): number {
+  return Math.cos(value / 360 * Tau);
+}
+export function cos(value: number, period: number, min: number, max: number): number {
+  return ((Math.cos(value / period * Tau) + 1) / 2) * (max - min) + min;
+}
+
+export function tanDegrees(value: number): number {
+  return Math.tan(value / 360 * Tau);
+}
+export function tan(value: number, period: number): number {
+  return Math.tan(value / period * Tau);
 }
